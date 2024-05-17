5f652d931932a0878492baa493d0d0ac48e71c38
abbosbey2002 <abbosbeyqudratov@gmail.com>
2024-05-17 17:27:28 +0500

    change page controller error

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -76,7 +76,7 @@ class PageController extends Controller
             'text_uz' => 'nullable|string',
             'text_ru' => 'nullable|string',
             'text_en' => 'nullable|string',
-            'slug' => 'required|string' . $page->id,
+            'slug' => 'required|string',
             'image' => 'nullable|file|max:2048',
         ]);
 
