a38ef1d5ed0274cc60a379bb29fc3ce1c44382c4
Quvonchbek_Jalilov <=>
2024-07-06 18:12:41 +0500

    admin

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -221,7 +221,7 @@
                             <a class="nav-link dropdown-toggle nav-user me-0 waves-effect waves-light" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                                 <img src="/admin-panel/assets/images/users/avatar-4.jpg" alt="user-image" class="rounded-circle">
                                 <span class="ms-1 d-none d-md-inline-block">
-                                    Jamie D. <i class="mdi mdi-chevron-down"></i>
+                                    Admin <i class="mdi mdi-chevron-down"></i>
                                 </span>
                             </a>
                             <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
